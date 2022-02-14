e5ec5fbbcc5130b16cec062ecf8f5336225bc53f
郁帅奇 <shuaiqi@kaixingame.cn>
2022-02-14 16:30:12 +0800

    add payment feign

---
+++ cloud-consume-order9002/src/main/java/com/genius/cloud/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.genius.cloud.controller;
+
+import com.genius.cloud.beans.Payment;
+import com.genius.cloud.common.CommonResult;
+import com.genius.cloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@RestController
+public class OrderController {
+
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+
+    @GetMapping(value = "/consumer/payment/timeout")
+    public CommonResult<String> feignTimeout() {
+        return paymentFeignService.feignTimeout();
+    }
+
+}
---
+++ cloud-consume-order9002/src/main/java/com/genius/cloud/service/PaymentFeignService.java
@@ -0,0 +1,20 @@
+package com.genius.cloud.service;
+
+import com.genius.cloud.beans.Payment;
+import com.genius.cloud.common.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-PAYMENT")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    CommonResult<String> feignTimeout();
+
+}
